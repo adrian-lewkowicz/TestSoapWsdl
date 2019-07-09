c22ed294e671d5265294ded799973746efee11d4
Adrian Lewkowicz <adrian.lewkowicz.biala@gmail.com>
2019-07-09 11:14:25 +0200

    test wsdl

--- TestSoapWsdl/Form1.cs
+++ TestSoapWsdl/Form1.cs
@@ -16,5 +16,12 @@ namespace TestSoapWsdl
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference.CalculatorSoapClient client = new ServiceReference.CalculatorSoapClient();
+            textBox3.Text = client.Add(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text)).ToString();
+
+        }
     }
 }
